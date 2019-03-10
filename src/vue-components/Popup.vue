21920802393fd2291cf9575dc0f14e5dcfca1b67
Javier <javierdwd@gmail.com>
2019-03-10 07:21:39 -0300

    Enhance results interface. Display additional information such as plural form, context, etc

--- src/vue-components/Popup.vue
+++ src/vue-components/Popup.vue
@@ -6,37 +6,7 @@
       <div class="indeterminate"></div>
     </div>
 
-    <PopupResults :linguee="linguee" v-if="showTranslation"/>
-    <keep-alive></keep-alive>
-
-    <div class="hide">
-      <h4 class="blue-text">{{ linguee.queryTerm }}</h4>
-
-      <dl v-for="(word, index) in linguee.words" :key="index">
-        <dt>
-          <span class="blue-text text-darken-4">{{ word.term }}</span>
-          ({{ word.type }})
-        </dt>
-
-        <dd v-for="(translation, index) in word.translations" :key="index">
-          <div class="translation">
-            <span class="translation__term">
-              <span class="green-text text-darken-4">{{ translation.term }}</span>
-              {{ translation.type ? `(${ translation.type })` : '' }}
-            </span>
-            <ul class="translation__examples" v-if="translation.examples">
-              <li v-for="(example, index) in translation.examples" :key="index">
-                <span class="translation__phrase blue-text text-accent-2">{{ example.phrase }}</span>
-                <br>
-                <span
-                  class="translation__translation green-text text-accent-2"
-                >{{ example.translation }}</span>
-              </li>
-            </ul>
-          </div>
-        </dd>
-      </dl>
-    </div>
+    <PopupResults :linguee="linguee" v-if="showTranslation"></PopupResults>
   </div>
 </template>
 
--- src/vue-components/popup/Results.vue
+++ src/vue-components/popup/Results.vue
@@ -1,31 +1,61 @@
 <template>
   <ul class="collapsible" ref="linguee-results">
     <li v-for="(word, index) in linguee.words" :key="index" :class="{ active: (index === 0) }">
-      <div class="collapsible-header">
-        <a class="dropdown-trigger" href="#" data-target="dropdown1">
+      <div class="collapsible-header word">
+        <a class="word__audio-trigger" href="#">
           <i class="tiny material-icons">volume_up</i>
         </a>
-        <span class="blue-text text-darken-4">{{ word.term }}</span>
-        ({{ word.type }})
-        <i
-          class="tiny material-icons"
-        >{{ index === 0 ? 'expand_less' : 'expand_more' }}</i>
+        <div class="aux-wrapper">
+          <span class="word__word blue-text text-darken-4">{{ word.term }}</span>
+          <span
+            class="word__context grey-text text-darken-1"
+            v-if="word.additionalInfo && word.additionalInfo.context"
+          >({{ word.additionalInfo.context }})</span>
+
+          <span class="word__type grey-text">{{ word.type }}</span>
+          <span
+            class="word__verbs grey-text text-darken-1"
+            v-if="word.additionalInfo && word.additionalInfo.verbs"
+          >[{{ word.additionalInfo.verbs.join(', ') }}]</span>
+          <span
+            class="word__plural grey-text text-darken-1"
+            v-if="word.additionalInfo && word.additionalInfo.plural"
+          >
+            (
+            <strong class="grey-text text-lighten-1">Plural:</strong>
+            {{ word.additionalInfo.plural }}
+            )
+          </span>
+        </div>
+
+        <i class="tiny material-icons expand-icon expand-less">more_horiz</i>
+        <i class="tiny material-icons expand-icon expand-more">expand_more</i>
       </div>
       <div class="collapsible-body">
-        <ul v-for="(translation, index) in word.translations" :key="index" class="collection">
-          <li class="collection-item">
-            <span class="translation__term">
-              <span class="green-text text-darken-4">{{ translation.term }}</span>
-              {{ translation.type ? `(${ translation.type })` : '' }}
+        <ul class="collection">
+          <li
+            v-for="(translation, index) in word.translations"
+            :key="index"
+            class="collection-item"
+          >
+            <span class="translation">
+              <span class="translation__term grey-text text-darken-4">{{ translation.term }}</span>
+              <span
+                class="translation__type grey-text text-darken-1"
+                v-if="translation.type"
+              >{{ translation.type }}</span>
             </span>
 
-            <div v-if="translation.examples">
-              <p v-for="(example, index) in translation.examples" :key="index">
-                <span class="translation__phrase blue-text text-accent-2">{{ example.phrase }}</span>
-                <br>
-                <span
-                  class="translation__translation green-text text-accent-2"
-                >{{ example.translation }}</span>
+            <div class="examples" v-if="translation.examples">
+              <p class="example" v-for="(example, index) in translation.examples" :key="index">
+                <span class="example__phrase grey-text text-darken-2">
+                  <i class="material-icons blue-text text-accent-4">subdirectory_arrow_left</i>
+                  {{ example.phrase }}
+                </span>
+                <span class="example__translation grey-text text-darken-2">
+                  <i class="material-icons green-text text-accent-4">subdirectory_arrow_right</i>
+                  {{ example.translation }}
+                </span>
               </p>
             </div>
           </li>
@@ -49,6 +79,8 @@ export default {
     destroyCollapsibleInst() {
       if (mCollapsibleInst) {
         mCollapsibleInst.destroy();
+
+        mCollapsibleInst = undefined;
       }
     }
   },
@@ -63,6 +95,3 @@ export default {
   }
 };
 </script>
-
-<style>
-</style>
