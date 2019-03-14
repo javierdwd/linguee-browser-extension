eb7938c0b3a0cd71b0f95abbbe522e1a06ecff73
Javier <javierdwd@gmail.com>
2019-03-14 15:05:13 -0300

    Implements a very basic search-and-translate form

--- src/vue-components/Popup.vue
+++ src/vue-components/Popup.vue
@@ -1,25 +1,34 @@
 <template>
   <div id="extension-popup">
-    <p v-if="!showTranslation">{{ message }}</p>
+    <p v-if="showMessage">{{ message }}</p>
 
     <div class="progress" v-if="lookingForTranslation">
       <div class="indeterminate"></div>
     </div>
 
     <results :linguee="linguee" v-if="showTranslation"></results>
+
+    <search-box v-if="!showTranslation && !lookingForTranslation"></search-box>
   </div>
 </template>
 
 <script>
-import PopupStore from "../scripts/PopupStore";
-import Results from "./popup/Results.vue";
+import PopupStore from "../utils/PopupStore";
+import Results from "./results/Results.vue";
+import SearchBox from "./SearchBox.vue";
 
 export default {
   data() {
     return PopupStore;
   },
   components: {
-    Results
+    Results,
+    SearchBox
+  },
+  computed: {
+    showMessage() {
+      return !this.showTranslation && this.message !== "";
+    }
   }
 };
 </script>
---
+++ src/vue-components/SearchBox.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="search-box">
+    <div class="input-field">
+      <i class="material-icons prefix">search</i>
+      <input id="icon_prefix" type="text" v-model="searchTerm" @keyup.enter="doSearch" autofocus>
+      <label for="icon_prefix">Translate...</label>
+    </div>
+  </div>
+</template>
+
+<script>
+import { lookForTranslation } from "../utils/Translator";
+
+export default {
+  data() {
+    return {
+      searchTerm: ""
+    };
+  },
+  methods: {
+    doSearch(term) {
+      lookForTranslation(this.searchTerm);
+
+      this.searchTerm = "";
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+#search-box {
+  padding-top: 5px;
+}
+</style>
+
